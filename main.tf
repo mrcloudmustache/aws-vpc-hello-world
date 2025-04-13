4636d2193f826b82ea2b4e7328e58c3dfc5b0c16
James Thomas <mrcloudmustache@gmail.com>
2025-04-13 11:10:53 -0500

    Updated module version

--- main.tf
+++ main.tf
@@ -1,6 +1,6 @@
 module "vpc-hello-world" {
   source  = "app.terraform.io/mrcloudmustache/vpc-hello-world/aws"
-  version = "1.0.0"
+  version = "1.0.1"
 
   cidr_block = "10.1.1.0/24"
   environment = "dev"
